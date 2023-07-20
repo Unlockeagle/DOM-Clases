635339a977927d365956e4ca281cfadfa6d5e838
unlockeagle <unlock.eagle@gmail.com>
2023-07-19 23:12:25 -0400

    DOM-getElemets

---
+++ app.js
@@ -0,0 +1,28 @@
+// const titulo = document.getElementById('titulo');
+// console.log(titulo.tagName);
+
+// const toppings = document.getElementsByClassName('toppings');
+// console.log(toppings[3]);
+
+// const misToppings = document.getElementsByTagName('li');
+// console.log(misToppings);
+
+// const fondoMarron = document.getElementsByClassName('fondo-marron');
+// console.log(fondoMarron);
+
+
+// // *******************************************************8
+// // toma el primer elemento con esa id o clace se puede combinar los selectores XD
+// const primerToppingNaranja = document.querySelector('ul li.fondo-naranja');
+// console.log(primerToppingNaranja);
+
+
+// // para no seleccionar una caracteristica
+// const primerToppingNoMarron = document.querySelector('ul li:not(.fondo-marron)');
+// console.log(primerToppingNoMarron);
+
+// // *********************************************************888
+// // retorna los elmentos como un arreglo, lista de nodos, usar length para ver la cantida de nodos en el arreglo 
+// const toppingsNaranja = document.querySelectorAll('.toppings.fondo-naranja');
+// console.log(toppingsNaranja.length);
+// console.log(toppingsNaranja[1]);
